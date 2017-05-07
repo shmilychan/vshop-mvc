728f7f0df27751d4bfcad64b69fde99d4405863e
czl <852995968@qq.com>
2017-05-08 01:12:16 +0800

    前台首页实现了ajax静态显示级联菜单

--- vshop-service/src/main/java/cn/mldn/vshop/service/back/IItemServiceBack.java
+++ vshop-service/src/main/java/cn/mldn/vshop/service/back/IItemServiceBack.java
@@ -1,11 +1,21 @@
 package cn.mldn.vshop.service.back;
 
 import java.util.List;
+import java.util.Map;
 
 import cn.mldn.vshop.vo.Item;
+import cn.mldn.vshop.vo.Subitem;
 
 public interface IItemServiceBack {
 
+	/**
+	 * 列出所有的一级菜单一级该菜单所含有的所有二级菜单信息：<br/>
+	 * 1.使用IItemDAO.findAll()方法查询出所有的一级栏目。<br/>
+	 * 2.根据所有查询所得的一级菜单找到该菜单包含的所有二级菜单。<br/>
+	 * @return Map集合的key为一级菜单信息，value为其对应的二级菜单信息。<br/>
+	 * @throws Exception SQL异常
+	 */
+	public Map<Item, List<Subitem>> listDetails() throws Exception;
 	/**
 	 * 实现所有的栏目信息列表，调用IItemDAO.findAll()
 	 * @return 所有的栏目数据
--- vshop-service/src/main/java/cn/mldn/vshop/service/back/impl/ItemServiceBackImpl.java
+++ vshop-service/src/main/java/cn/mldn/vshop/service/back/impl/ItemServiceBackImpl.java
@@ -1,16 +1,35 @@
 package cn.mldn.vshop.service.back.impl;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import cn.mldn.util.factory.Factory;
 import cn.mldn.vshop.dao.IItemDAO;
+import cn.mldn.vshop.dao.ISubitemDAO;
 import cn.mldn.vshop.service.abs.AbstractService;
 import cn.mldn.vshop.service.back.IItemServiceBack;
 import cn.mldn.vshop.vo.Item;
+import cn.mldn.vshop.vo.Subitem;
 
 public class ItemServiceBackImpl extends AbstractService
 		implements
 			IItemServiceBack {
+	@Override
+	public Map<Item, List<Subitem>> listDetails() throws Exception {
+		Map<Item, List<Subitem>> map = new HashMap<>();
+		IItemDAO itemDAO = Factory.getDAOInstance("item.dao");
+		ISubitemDAO subitemDAO = Factory.getDAOInstance("subitem.dao");
+		List<Item> allItems = itemDAO.findAll();//查询出所有的一级菜单
+		Iterator<Item> iter = allItems.iterator();
+		while (iter.hasNext()) {
+			Item item = iter.next();
+			map.put(item, subitemDAO.findAllByItem(item.getIid()));
+		}
+		
+		return map;
+	}
 	@Override
 	public boolean edit(Item vo) throws Exception {
 		IItemDAO itemDAO = Factory.getDAOInstance("item.dao");
